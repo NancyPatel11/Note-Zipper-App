5f8e11614040fa050b37af64b74176a17f95ca39
NancyPatel11 <nancypatel5757@gmail.com>
2023-12-24 16:33:37 +0530

    added UI for landing page, header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LandingPage from "./screens/Landing/LandingPage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main style={{ minHeight: "93vh" }}>
+        <LandingPage />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/screens/Landing/LandingPage.js
@@ -0,0 +1,34 @@
+import { Button, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./LandingStyles.css";
+
+function LandingPage() {
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to Note Zipper</h1>
+              <p className="subtitle">One safe place for all your notes</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button size="lg" className="landingbutton" variant="outline-primary">
+                  Sign Up
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default LandingPage;
