ab0fc1ac09b943d2181cb626e11759520b5f2676
NancyPatel11 <nancypatel5757@gmail.com>
2023-12-24 15:48:33 +0530

    initialized backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`server running on port ${PORT}`));
