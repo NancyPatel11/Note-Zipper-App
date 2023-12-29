dad154f504add2895b512e3d29ef8d2a5e52c4fc
NancyPatel11 <nancypatel5757@gmail.com>
2023-12-29 13:17:00 +0530

    added code for deployement

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
+const path = require("path");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
@@ -16,6 +17,23 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// -------------------------------deployement-------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API Running Successfully");
+  });
+}
+// -------------------------------deployement-------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
